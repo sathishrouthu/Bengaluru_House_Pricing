35f2076fd37e4cc0151239126f94c359c3d66474
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-08-06 17:03:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
 from pydoc import render_doc
 from urllib import response
 from flask import Flask,redirect,jsonify,request,render_template
-import util
+import pickle
+import json
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
 
 app = Flask(__name__)
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index=-1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0],2)
+
+
+def get_locations_for_ui():
+    return __locations
+
+def load_saved_artifats():
+    print("Loading... saved artifacts.....")
+    global __data_columns
+    global __locations
+    global __model
+    with open("./artifacts/columns.json","r") as f:
+        __data_columns = json.load(f)["data_columns"]
+        __locations = __data_columns[3:]
+    with open("./artifacts/LR_model.pkl","rb") as f:
+        __model = pickle.load(f)
+    print("Loaded Artifacts done...")
+
 
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        'locations':util.get_location_names()
+        'locations':get_locations_for_ui()
     })
     response.headers.add("Access-Control-Allow-Origin",'*')
     return response
@@ -19,12 +56,12 @@ def predict_home_price():
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
     response = jsonify({
-        'estimated_price':util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price':get_estimated_price(location,total_sqft,bhk,bath)
     })
     response.headers.add("Access-Control-Allow-Origin",'*')
     return response
 
 if __name__=="__main__":
-    util.load_saved_artifats()
+    load_saved_artifats()
     print("Starting Flask server....")
-    app.run()
+    app.run(debug=True)
