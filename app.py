0fbe6d9ec2762beabb80877f2ab43edd82eb844a
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2022-08-07 23:19:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,redirect,jsonify,request,render_template
+from flask import Flask,jsonify,request
 import pickle
 import json
 import numpy as np
 
-__locations = None
-__data_columns = None
-__model = None
+__data_columns = pickle.load(open("./artifacts/data_columns.pkl","rb"))["data_columns"]
+__locations = __data_columns[3:]
+__model = pickle.load(open("./artifacts/LR_model.pkl","rb"))
 
 app = Flask(__name__)
 
@@ -24,32 +24,26 @@ def get_estimated_price(location,sqft,bhk,bath):
     return round(__model.predict([x])[0],2)
 
 
-def get_locations_for_ui():
-    return __locations
+# def get_locations_for_ui():
+#     global __locations
+#     return __locations
 
-def load_saved_artifats():
-    print("Loading... saved artifacts.....")
-    global __data_columns
-    global __locations
-    global __model
-    with open("./artifacts/data_columns.pkl","rb") as f:
-        __data_columns = pickle.load(f)["data_columns"]
-        __locations = __data_columns[3:]
-    with open("./artifacts/LR_model.pkl","rb") as f:
-        __model = pickle.load(f)
-    print("Loaded Artifacts done...")
+# def load_saved_artifats():
+#     print("Loading... saved artifacts.....")
+#     print("Loaded Artifacts done...")
 
 
 @app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
-        'locations':get_locations_for_ui()
+        'locations':__locations
     })
     response.headers.add("Access-Control-Allow-Origin",'*')
     return response
 
 @app.route('/predict_home_price',methods=["GET","POST"])
 def predict_home_price():
+    global __model
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
     bhk = int(request.form['bhk'])
@@ -61,7 +55,7 @@ def predict_home_price():
     return response
 
 if __name__=="__main__":
-    load_saved_artifats()
+    # load_saved_artifats()
     print("Starting Flask server....")
     app.run(debug=True)
     
